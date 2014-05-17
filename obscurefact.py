d4b58a2ccfdc5ebeddf5deaf2c8729685c1341e1
Frances Hocutt <frances.hocutt@gmail.com>
2014-05-17 00:25:58 -0700

    added support for wikidata
    added "wikidata" option; bot now tweets out random wikidata claims associated
    with one of the 50 most-recently-changed wikidata items.
    
    note: there is still no guarantee that a property or the item associated
    with a claim's value will have a label; will fail with a KeyError in that case

--- obscurefact.py
+++ obscurefact.py
@@ -42,7 +42,77 @@ def wikipediarecentchange():
 ############
 # wikidata
 
-#TODO
+
+def wikidata(): 
+    recentchanges = wikidatarecentchanges()
+#random title from the list, then English label and list of claims
+    entity = recentchanges[random.randint(0, len(recentchanges)-1)]["title"]
+
+    entitydata = getlabelclaims(entity)
+
+
+    while (("claims" not in entitydata["entities"][entity]) or ("labels" not in entitydata["entities"][entity])): #TODO: split this line somehow
+
+        entity = recentchanges[random.randint(0, len(recentchanges)-1)]["title"]
+        entitydata = getlabelclaims(entity)
+        #might want to put in a guarantee this ends someday...naaaah. #TODO
+
+    label = entitydata["entities"][entity]["labels"]["en"]["value"]
+    claims = entitydata["entities"][entity]["claims"]
+
+    prop, claimvalue = randomclaim(claims)
+
+    return (label, prop, claimvalue)
+
+#API query to get the 100 most recent changes
+def wikidatarecentchanges():
+    URI = "http://wikidata.org//w/api.php?action=query&list=recentchanges&format=json&rcnamespace=0&rcprop=title&rcshow=!minor|!bot|!redirect&rclimit=100&rctype=edit|new"
+    r = requests.get(URI, headers = headers)
+    rtext = r.json()
+    recentchanges = rtext["query"]["recentchanges"]
+    return recentchanges
+
+
+#finds the first English label of a given item
+def findlabel(entity):
+
+    URI = "http://wikidata.org/w/api.php?action=wbgetentities&format=json&ids=%s&props=labels&languages=en" % entity
+    r = requests.get(URI, headers = headers)
+    rtext = r.json()
+
+    print rtext
+
+    label = rtext["entities"][entity]["labels"]["en"]["value"]
+
+    return label
+
+
+#finds the label of a given item and a list of associated claims
+def getlabelclaims(entity):
+
+    URI = "http://wikidata.org/w/api.php?action=wbgetentities&format=json&ids=%s&props=labels|claims&languages=en&ungroupedlist=" % entity
+    r = requests.get(URI, headers = headers)
+    rtext = r.json()
+    print rtext
+
+    return (rtext)
+
+
+def randomclaim(claims):
+    print "\n\nclaims:\n"+str(claims)
+    randclaim = claims[0]
+    if len(claims) > 1:
+        randclaim = claims[random.randint(0, len(claims)-1)]
+    prop = randclaim["mainsnak"]["property"]
+    proplabel = findlabel(prop)    #FAILS if prop has no label
+    claimvalue = randclaim["mainsnak"]["datavalue"]["value"]
+    claimtype = randclaim["mainsnak"]["datavalue"]["type"]
+
+    if claimtype == "wikibase-entityid":
+         claimvalue = findlabel("Q"+ str(claimvalue["numeric-id"])) # so very wasteful... FIXME
+
+    return proplabel, claimvalue
+
 
 #############
 # wikivoyage
@@ -124,25 +194,34 @@ def choosesite():
 
     # which site do we query?
     site = raw_input("Would you like information from Wikipedia, Wikivoyage, \
-or Wikibooks? ") #could insert these from a separate list for easier maintenance
+Wikidata, or Wikibooks?\n") #could insert these from a separate list for easier maintenance
     site = string.lower(site) #handling case
     site = string.strip(site) #removing whitespace
 
     if site == "wikipedia":
-        recentchange = wikipediarecentchange()
-        sentence = findobscuresentence(recentchange[1])
-
-        return ("WP", recentchange[0], sentence) # title, extracted sentence
+        topic, text = wikipediarecentchange()
+        sentence = findobscuresentence(text)
+        obscurefact = "%s: %s" % (topic, sentence)
+        return ("WP", obscurefact)
 
     elif site == "wikivoyage":        
-        recentchange = wikivoyagerecentchange()
-        sentence = findobscuresentence(recentchange[1])
-
-        return ("WV", recentchange[0], sentence)   # title, extracted sentence
+        location, text = wikivoyagerecentchange()
+        sentence = findobscuresentence(text)
+        obscurefact = "%s: %s" % (location, sentence)
+        return ("WV", obscurefact)
 
     elif site == "wikibooks":
-	recentchange = wikibooksrecentchange()
-        return ("WB", recentchange[0], recentchange[1])
+	title, section = wikibooksrecentchange()
+        if title == section: 
+            obscurefact = title
+        else:
+            obscurefact = "%s: %s" % (title, section)
+        return ("WB", obscurefact)
+
+    elif site == "wikidata":
+        label, prop, claimvalue = wikidata() 
+        obscurefact = "(%s) %s (%s)" % (label, prop, claimvalue)
+        return ("WD", obscurefact )
 
     else:  #re-prompt
         print "Please enter one of the options in the list or hit ctrl+C to end this program. \n"
@@ -179,26 +258,21 @@ def findobscuresentence(pagetext): # expects Wikipedia-style plaintext
 ##    return sentence
 
 
-def tweetsentence(site, topic, sentence):
-    if topic is sentence: #possible in wikibooks (when no subsections)
-        info = topic      #only tweet the title once
-    else:
-        info = "(" +site + ") " + topic + ": " + sentence + "."
+def tweet(site, obscurefact):
+    tweet = "(%s) %s" % (site, obscurefact)
 
-    if len(info) > 140:   #stay inside twitter's character limit
-        tweet = (info[:139]) 
-    else:
-        tweet = info
-        
+    if len(obscurefact) > 140:   #shorten to <=140 chars
+        tweet = tweet[:139]
+
     api.PostUpdate(tweet)
 
 
 #Get the user to choose a site to tweet out, tweet the result, and print it 
 #to the command line for immediate gratification/debugging.
 def run():
-    site, topic, sentence = choosesite()
-    tweetsentence(site, topic, sentence)
-    print "(" + site + ") " + topic + ": " + sentence + "."
+    site, obscurefact = choosesite()
+    tweet(site, obscurefact)
+    print "(%s) %s\n" % (site, obscurefact)
 
 
 if __name__ == "__main__":
